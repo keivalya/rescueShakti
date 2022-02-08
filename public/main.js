77a76394d0a498185d796314228b51de5c5da2a8
sairaj <sairaj2119@gmail.com>
2022-02-08 12:39:07 +0530

    remove mobile number

--- public/main.js
+++ public/main.js
@@ -2,7 +2,7 @@ const fakecallbtn = document.getElementById('fakecall')
 const chatbotBtn = document.getElementById('chatbot')
 
 fakecallbtn.addEventListener('click', () => {
-  const num = '+918332826244'
+  // const num = '+918332826244'
   fetch('/fakecall', {
     method: 'POST',
     body: JSON.stringify({ phoneNumber: num }),
